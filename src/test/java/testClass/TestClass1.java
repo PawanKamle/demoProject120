87f4e3b7b6ce41e0a6f42e39d478803891ee269c
pawan522 <kamlepawan111@gmail.com>
2022-11-20 23:34:21 +0530

    first commmit

---
+++ src/test/java/testClass/TestClass1.java
@@ -0,0 +1,45 @@
+package testClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+	public static WebDriver driver;
+
+	@BeforeMethod
+	public void launchDriver() {
+
+		System.setProperty("webdriver.chrome.driver", "c:\\browserdriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.navigate().to("http://AutomationTalks.com/");
+	}
+
+	@Test
+	public void test1() {
+
+		System.out.println("test 1 title = " + driver.getTitle());
+
+	}
+
+	@Test
+	public void test2() {
+
+		System.out.println("test 2 title = " + driver.getTitle());
+
+	}
+
+	@Test
+	public void test3() {
+
+		System.out.println("test 3 title = " + driver.getTitle());
+	}
+}
